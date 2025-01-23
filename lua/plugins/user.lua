c424ea21277f685323fae9a2a572c10a60bae200
sagar prakash kawad <sagzone@sagars-MacBook-Air.local>
2025-01-23 23:32:45 +0530

    minor changes(not working)

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -45,11 +45,18 @@ return {
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
     "L3MON4D3/LuaSnip",
+
+    requires = { "rafamadriz/friendly-snippets" }, -- Snippets for ES7+/React/Redux/React-Native
+
     config = function(plugin, opts)
       require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
+      require("luasnip.loaders.from_vscode").lazy_load {}
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
+
+      -- You can also extend for HTML if needed, although it's typically handled by friendly-snippets
+      luasnip.filetype_extend("html", { "html" })
     end,
   },
 
