cff6bac8c6646c9ea1042c3d281c482ef0ff3dd2
sagar prakash kawad <sagzone@sagars-MacBook-Air.local>
2025-04-15 01:36:50 +0530

    added flutter-tools

---
+++ lua/plugins/flutter-tools.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "akinsho/flutter-tools.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- Optional (for UI improvements)
+    },
+    config = function() require("flutter-tools").setup {} end,
+  },
+}
